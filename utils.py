d292da24fbd85074b371345527d1f1f48460f83a
Robert George <rgeorge@midnightweb.net>
2020-01-15 15:59:42 -0800

    Altered handling of {@dice ...} tag

--- utils.py
+++ utils.py
@@ -48,7 +48,7 @@ def remove5eShit(s):
     s = re.sub(r'{@h}', r'', s)
     s = re.sub(r'{@recharge (.*?)}', r'(Recharge \1-6)', s)
     s = re.sub(r'{@recharge}', r'(Recharge 6)', s)
-    s = re.sub(r'{@dice (.*?)}', r'\1', s)
+    s = re.sub(r'{@dice (.*?)(\|.*?)}', r'\1', s)
     s = re.sub(r'{@scaledice (.*?)\|(.*?)\|(.*?)}', r'\3', s)
     s = re.sub(r'{@filter (.*?)(\|.*?)?}', r'\1', s)
     s = re.sub(r'{@\w+ ([^{]*?)(\|[^{]*?)?}', r'\1', s)
